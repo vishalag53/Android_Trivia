2b562d18db7d4afc90fdf24d24bad55a5e0ba495
vishalag53 <vishal.agrawal1207@gmail.com>
2023-05-17 21:57:11 +0530

    Add

---
+++ app/src/main/java/com/example/androidtrivia/TitleFragment.kt
@@ -0,0 +1,30 @@
+package com.example.androidtrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.androidtrivia.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener(
+            //view:View -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment3)
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment3))
+        return binding.root
+    }
+
+}
