e92746108e83a10f5216fea0b6601f0db35b6bc6
vishalag53 <vishal.agrawal1207@gmail.com>
2023-05-18 10:48:58 +0530

    Button action add

--- app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
@@ -5,55 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.androidtrivia.databinding.FragmentGameWonBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GameWonFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GameWonFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_won, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameWonFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameWonFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val binding: FragmentGameWonBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchBtn.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment3)
+        }
+        return binding.root
     }
 }
